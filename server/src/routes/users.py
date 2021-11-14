99c038e4dbd8fdb69e2066a45679c497b33c1eae
Redeltaz <lucascampistron95@gmail.com>
2021-11-14 19:09:42 +0100

    add request as class, init user requests

---
+++ server/src/routes/users.py
@@ -0,0 +1,71 @@
+from fastapi import APIRouter, HTTPException
+from typing import List
+
+from ..crud import user_crud
+from ..schemas import User, UserCreate, UserUpdate
+
+user_router = APIRouter()
+
+@user_router.get("/", response_model=List[User])
+def get_users():
+    """
+    get all users
+    """
+    users_db = user_crud.read_all()
+    
+    return users_db
+
+@user_router.get("/{user_id}", response_model=User)
+def get_user(user_id: str):
+    """
+    get user by id
+    """
+    user_db = user_crud.read(user_id)
+    if not user_db:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return user_db
+
+@user_router.post("/", response_model=User)
+def create_user(request_body: UserCreate):
+    """
+    create user
+    """
+    user_params = dict(request_body)
+    user_params["is_superuser"] = False
+    user_params["is_admin"] = False
+    
+    user_db = user_crud.create(user_params)
+    
+    return user_db
+
+@user_router.put("/{user_id}", response_model=User)
+def update_user(
+    user_id: str,
+    request_body: UserUpdate
+):
+    """
+    update user
+    """
+    user_params = dict(request_body)
+    
+    user_db = user_crud.read(user_id)
+    if not user_db:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    user_db = user_crud.update(user_id, user_params)
+    
+    return user_db
+
+@user_router.delete("/{user_id}")
+def delete_user(user_id: str):
+    """
+    delete user
+    """
+    user_db = user_crud.read(user_id)
+    if not user_db:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    user_db = user_crud.delete(user_id)
+    
+    return user_db
